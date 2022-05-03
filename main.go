6796cab6294254178bb85dc3f2bdf2da7cb73a8c
George Paul <george@pollardpaul.com>
2022-05-03 23:12:38 +0200

    added csv reader

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("The project begins...")
+	open_csv("Reduced Features for TAI project.csv")
+
+}
+
+func open_csv(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file", err)
+	}
+	reader := csv.NewReader(file)
+	records, _ := reader.ReadAll()
+	fmt.Println(records)
 }
