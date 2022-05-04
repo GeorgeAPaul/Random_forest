07f109e05081c9399f7a9ad08c566dbd70c4a5ae
George Paul <george@pollardpaul.com>
2022-05-04 22:03:48 +0200

    open_csv func added

--- main.go
+++ main.go
@@ -3,21 +3,39 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("The project begins...")
-	open_csv("Reduced Features for TAI project.csv")
+	fmt.Println(open_csv_column("Reduced Features for TAI project.csv", 4))
+	//open_csv_column("test.csv", 1)
 
 }
 
-func open_csv(path string) {
-	file, err := os.Open(path)
-	if err != nil {
-		fmt.Println("Error opening file", err)
-	}
+func open_csv_column(path string, column int) (a []float64) {
+
+	file, _ := os.Open(path)
 	reader := csv.NewReader(file)
-	records, _ := reader.ReadAll()
-	fmt.Println(records)
+
+	var data []float64
+
+	//deal with header
+	_, _ = reader.Read()
+
+	for {
+
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		record_float, _ := strconv.ParseFloat(record[column], 64)
+
+		data = append(data, record_float)
+	}
+
+	return data
 }
