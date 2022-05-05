d25589471ec84713d6f7d5a9bb463e8471177f8c
George Paul <george@pollardpaul.com>
2022-05-05 16:26:59 +0200

    gini_index

--- main.go
+++ main.go
@@ -4,38 +4,89 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strconv"
 )
 
 func main() {
 	fmt.Println("The project begins...")
-	fmt.Println(open_csv_column("Reduced Features for TAI project.csv", 4))
-	//open_csv_column("test.csv", 1)
+	//col := open_csv_column("Reduced Features for TAI project.csv", 4)
+	//label := open_csv_column("Reduced Features for TAI project.csv", 151)
+	data := open_csv("test.csv")
+	fmt.Println(data)
+	fmt.Print(gini_index(data, 0))
 
 }
 
-func open_csv_column(path string, column int) (a []float64) {
+func open_csv(path string) (data [][]float64) {
 
 	file, _ := os.Open(path)
 	reader := csv.NewReader(file)
 
-	var data []float64
-
 	//deal with header
 	_, _ = reader.Read()
 
-	for {
+	for i := 0; i >= 0; i++ {
 
 		record, err := reader.Read()
 		if err == io.EOF {
 			break
 		}
 
-		record_float, _ := strconv.ParseFloat(record[column], 64)
+		data = append(data, make([]float64, 0))
 
-		data = append(data, record_float)
+		for column := range record {
+			record_float, _ := strconv.ParseFloat(record[column], 64)
+			data[i] = append(data[i], record_float)
+		}
+	}
+	return data
+}
+
+func gini_index(data [][]float64, column int) (gini_index float64) {
+
+	threshold := 5.5
+	//total := float64(len(data))
+
+	var rows_above [][]float64
+	var rows_below [][]float64
+
+	above_label0 := 0.
+	below_label0 := 0.
+	above_label1 := 0.
+	below_label1 := 0.
+
+	for _, row := range data {
+
+		if row[column] < threshold {
+			rows_below = append(rows_below, row)
+
+			if row[len(row)-1] == 0 {
+				below_label0++
+			} else {
+				below_label1++
+			}
+		} else {
+			rows_above = append(rows_above, row)
+
+			if row[len(row)-1] == 0 {
+				above_label0++
+			} else {
+				above_label1++
+			}
+		}
 	}
 
-	return data
+	total_above := above_label0 + above_label1
+	total_below := below_label0 + below_label1
+
+	fmt.Printf("Above %v\n", rows_above)
+	fmt.Printf("Below %v\n", rows_below)
+
+	gini_above := 1 - math.Pow(above_label0/total_above, 2) - math.Pow(above_label1/total_above, 2)
+	gini_below := 1 - math.Pow(below_label0/total_below, 2) - math.Pow(below_label1/total_below, 2)
+
+	average_gini := (gini_above + gini_below) / 2
+	return average_gini //gini
 }
